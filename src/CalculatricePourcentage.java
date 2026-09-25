b8c2ce57fa67d9e48a6864b4b4299a44d7ea6bc5
SARAAA-cmd <saraaahyane@gmail.com>
2026-09-25 22:15:45 +0000

    Ajout de la methode appliquer remise

--- src/CalculatricePourcentage.java
+++ src/CalculatricePourcentage.java
@@ -3,4 +3,8 @@ public class CalculatricePourcentage {
     public double pourcentage(double valeur, double taux) {
         return valeur * taux / 100;
     }
+
+    public double appliquerRemise(double prix, double taux) {
+        return prix - pourcentage(prix, taux);
+    }
 }
