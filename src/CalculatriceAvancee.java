ca1331732880cac82962dedcb98474cb60a7572a
SARAAA-cmd <saraaahyane@gmail.com>
2026-09-25 21:27:41 +0000

    Ajout de la methode racine carree

--- src/CalculatriceAvancee.java
+++ src/CalculatriceAvancee.java
@@ -3,4 +3,11 @@ public class CalculatriceAvancee {
     public double puissance(double a, double b) {
         return Math.pow(a, b);
     }
+
+    public double racine(double a) {
+        if (a < 0) {
+            throw new IllegalArgumentException("Nombre negatif");
+        }
+        return Math.sqrt(a);
+    }
 }
